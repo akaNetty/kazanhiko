8db836f43faa943b359bf01dcfe68913eb72b0d3
NETTY <>
2018-05-16 22:58:28 +0900

    タイトル画面のBGM追加

--- Assets/Scripts/title.cs
+++ Assets/Scripts/title.cs
@@ -9,6 +9,7 @@ public class title : MonoBehaviour {
 	bool go = false;
 	public AudioSource okSound;
 	public AudioSource backSound;
+	public AudioSource titlebgm;
 	public GameObject ufo;
 	public Animator ufoaway;
 	public GameObject selectbutton;
@@ -72,6 +73,7 @@ public class title : MonoBehaviour {
 		hitori.SetActive (false);
 		zenkoku.SetActive (false);
 		zensta3.SetActive (false);
+		titlebgm.Play ();
 	}
 	
 	// Update is called once per frame
